372050ffc9cb7fb53e228b1c552427bb008e6bd9
harcuracy <akandesoji4crist@gmail.com>
2023-10-25 15:20:29 +0100

    requirement & installation done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "-location-mention-recogniton"
+AUTHOR_USER_NAME = "harcuracy"
+SRC_REPO = "locationMentionR ecognition"
+AUTHOR_EMAIL = "akandesoji4christ@gmnail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="a small python package for nlp app",
+    long_description=long_description,
+    Long_description_content="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={"Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"},
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
--- template.py
+++ template.py
@@ -2,14 +2,14 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]:%(message)s:")
 
 
-project_name = "locationMentionR ecognition"
+project_name = "locationMentionRecognition"
 
 
-list_of_files =[
-   ".github/workflows/.gitkeep",
+list_of_files = [
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -27,9 +27,7 @@ list_of_files =[
     "requirement.txt",
     "setup.py",
     "research/trials.ipynb",
-    "test.py"
-
-   
+    "test.py",
 ]
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -38,13 +36,9 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)== 0):
-        with open (filepath,'w') as f:
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
             pass
             logging.info(f"creating empty file: {filepath}")
     else:
         logging.info(f"creating empty file: {filepath}")
-
-
-
-
